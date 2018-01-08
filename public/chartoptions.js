c8fa59993b8fb3896cd56e82c70830bcbf5f677a
Milan <milanvanas@gmail.com>
2018-01-09 00:35:11 +0100

    Emptied chartoptions, added demo.chartoptions

--- public/chartoptions.js
+++ public/chartoptions.js
@@ -1,34 +1,3 @@
 window.charts = [
-
-    // Taart 1
-    {
-        title: {
-            text: "Voorbeeld 1"
-        },
-        series: [{
-            name: "Aantal",
-            data: [
-                ['A', 88.19],
-                ['B', 9.22],
-                ['C', 1.58],
-                ['D', 1.01]
-            ]
-        }],
-    },
-
-    // Taart 2
-    {
-        title: {
-            text: "Voorbeeld 2"
-        },
-        series: [{
-            name: "Aantal",
-            data: [
-                ['A', 77.83],
-                ['B', 17.8],
-                ['C', 2.94],
-                ['D', 0.72],
-            ]
-        }],
-    },
+  
 ];
